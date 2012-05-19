238caeb0c74412e323c0edd87a6f25ca04c98fb3
Nathan Gass <gass@search.ch>
2012-05-19 12:31:40 +0200

    Actually compile. Implemented --caseinsensitive.

--- buniq.hs
+++ buniq.hs
@@ -9,6 +9,7 @@ import Data.BloomFilter.Easy (suggestSizing)
 import Data.BloomFilter.Hash (cheapHashes)
 import qualified System.Console.CmdArgs as CMD
 import System.Console.CmdArgs ((&=))
+import Data.Char
 
 data Args = Args { doubles          :: Bool
                  , caseinsensitive  :: Bool
@@ -40,7 +41,7 @@ defaults = Args { doubles = False
                                                  ++ "conjunction with the -f option, the first chars "
                                                  ++ "characters after the first num fields will be "
                                                  ++ "ignored. Character numbers are one based, i.e., "
-                                                 ++ "the first character is character one."
+                                                 ++ "the first character is character one.")
                 , verbose = False
                             &= CMD.name "v"
                             &= CMD.help "Verbose output to stderror."
@@ -56,21 +57,31 @@ main = do
   args <- CMD.cmdArgs defaults
   content <- if (inputFile args /= "") then readFile (inputFile args) else getContents
   when (verbose args)
-    $ hPutStrLn stderr $ "size: " ++ show (size `div` (8 * 1024)) ++ " hashnum: " ++ show hashnum
+    $ hPutStrLn stderr ( "size: " ++ show (size `div` (8 * 1024))
+                       ++ " hashnum: " ++ show hashnum
+                       )
   mapM_ putStrLn (filter args $ lines content)
   where
     (size, hashnum)  = suggestSizing (8 * 1024 * 1024) 0.0001
     hashes           = cheapHashes hashnum
-    filter args      = if doubles args
-                       then bloomFilter not hashes size . bloomFilter id hashes size
-                       else bloomFilter not hashes size
+    trans     args   = if caseinsensitive args then map toLower else id
+    onefilter args   = bloomFilter (trans args) hashes size
+    filter    args   = if doubles args
+                       then onefilter args not . onefilter args id
+                       else onefilter args not
 
-bloomFilter :: (Bool -> Bool) -> (String -> [Hash]) -> Int -> [String] -> [String]
-bloomFilter func hashes size xs = runST $ do
+bloomFilter :: (String -> String)  -- mapping function for strings
+            -> (String -> [Hash])  -- hash functions
+            -> Int                 -- size of bitarray
+            -> (Bool -> Bool)      -- bool function on result
+            -> [String]            -- strings to filter
+            -> [String]
+bloomFilter trans hashes size func xs = runST $ do
   bloom <- strictToLazyST $ newMB hashes size
   filterM (filter bloom) xs
   where
     filter bloom x = strictToLazyST $ do
-      known <- x `elemMB` bloom
-      insertMB bloom x
+      let x' = trans x
+      known <- x' `elemMB` bloom
+      insertMB bloom x'
       return $ func known
